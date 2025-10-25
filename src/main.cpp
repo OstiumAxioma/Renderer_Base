a61cbcca302276434f5cee8c2c2b0f4857764d0a
OstiumAxioma <ostiumaxioma@outlook.com>
2025-10-25 17:05:59 +0800

    VBO->VAO管线

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ void prepareVBO() {
 	GL_CALL(glDeleteBuffers(3, VBOs));
 }
 
-void prepare() {
+void bindVBOtoBuffer() {
     //准备顶点数据
     float vertices[] = {
         //位置属性
@@ -51,6 +51,77 @@ void prepare() {
 	GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
 }
 
+//单一存储
+void prepareSingleBuffer() {
+
+    //1.1 准备顶点位置数据
+    float position[] = {
+        //位置属性
+         0.5f,  0.5f, 0.0f,//右上角
+         0.5f, -0.5f, 0.0f,//右下角
+        -0.5f, -0.5f, 0.0f,//左下角
+	};
+
+    //1.2 准备顶点颜色数据
+    float color[] = {
+        //颜色属性
+        1.0f, 0.0f, 0.0f, //右上角 红色
+        0.0f, 1.0f, 0.0f, //右下角 绿色
+        0.0f, 0.0f, 1.0f, //左下角 蓝色
+	};
+
+	//2.1 分别生成单个VBO
+	GLuint positionVBO = 0;
+	GLuint colorVBO = 0;
+
+    GL_CALL(glGenBuffers(1, &positionVBO));
+    GL_CALL(glGenBuffers(1, &colorVBO));
+
+	//3. 分别填充数据到两个VBO
+	GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, positionVBO));
+	GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(position), position, GL_STATIC_DRAW));
+
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, colorVBO));
+    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW));
+
+    //4.1 生成VAO
+    GLuint VAO = 0;
+    GL_CALL(glGenVertexArrays(1, &VAO));
+
+    //4.2 绑定启用VAO
+    GL_CALL(glBindVertexArray(VAO));
+
+    //5.1 填充描述信息到VAO
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, positionVBO)); //之前为了填充颜色数据，换绑到了颜色VBO，现在我们要换回位置VBO
+    GL_CALL(glEnableVertexAttribArray(0)); //激活VAO的0号槽位用于存放
+    GL_CALL(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0)); //解读为在0号槽位，压入3个来自position的数据，都是GL_FLOAT数据格式，归一化关闭，用3个浮点数的步长位移来读取，不使用任何偏移量
+
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, colorVBO)); //换绑颜色VBO
+    GL_CALL(glEnableVertexAttribArray(1)); //激活VAO的1号槽位用于存放
+    GL_CALL(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
+
+    //5.2 解绑VAO
+    glBindVertexArray(0);
+}
+
+//交叉存储
+void prepareInterleavedBuffer() {
+    //1. 定义单个数据集
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, //右上角 红色
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, //右下角 绿色
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, //左下角 蓝色
+    };
+
+    //2. 生成单个VBO
+    GLuint verticeVBO = 0;
+    GL_CALL(glGenBuffers(1, &verticeVBO));
+
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, verticeVBO));
+    GL_CALL(glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW));
+
+}
+
 int main()
 {
     if (!app->init(800, 600)) {
@@ -68,7 +139,7 @@ int main()
     glViewport(0, 0, 800, 600); //视口起点，大小
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //画布清理颜色
 
-	prepareVBO();
+    prepareSingleBuffer();
 
     while (app->update()) {
         GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
