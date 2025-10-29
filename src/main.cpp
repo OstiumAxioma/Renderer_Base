bf3fb6883c033201cba8021bb438c84942ae6d08
OstiumAxioma <ostiumaxioma@outlook.com>
2025-10-29 17:24:59 +0800

    修改顶点着色器传递颜色数据到片元着色器

--- src/main.cpp
+++ src/main.cpp
@@ -59,17 +59,21 @@ void prepareShader () {
 	const char* vertexShaderSource =
         "#version 460 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 vColor;\n"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"   vColor = aColor;\n"
 		"}\0";
 
     const char* fragmentShaderSource =
         "#version 460 core\n"
+		"in vec3 vColor;\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "   FragColor = vec4(vColor, 1.0f);\n"
         "}\0";
 
 	//2. 创建顶点着色器对象并编译
